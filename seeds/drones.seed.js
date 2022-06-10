42b42eed6aec2c9c3d729474cbb2259d1fc37a2b
Daniel Gonzalez <danielgnzch@gmail.com>
2022-06-10 20:44:45 +0200

    Iteration 1 done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .then(() => {
+    return Drone.create(drones);
+  })
+  .then((drone) => {
+    console.log(
+      `<<< CREATE OPERATION! >>> ${drone.length} total drones inserted successfully!`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  })
+  .finally(() => {
+    mongoose.disconnect();
+  });
